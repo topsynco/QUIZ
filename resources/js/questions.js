dd9598b3be16f31acb226f3a33ea7a50497e1492
topsynco <68435582+topsynco@users.noreply.github.com>
2020-11-16 21:50:26 -0500

    Add files via upload

---
+++ resources/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Capital Of Connecticut:",
+        choices: ["Bridgeport", "Hartford", "New London", "Waterbury"],
+        answer: "Hartford"
+    },
+    {
+        title: "Capital Of Massachuesetts:",
+        choices: ["Boston", "Lawrence", "Springfield", "Worcester"],
+        answer: "Boston"
+    },
+    {
+        title: "Capital Of Vermont:",
+        choices: ["Rutland", "Burlington", "Barre", "Montpelier"],
+        answer: "Montpelier"
+    },
+    {
+        title: "Capital Of New Hampshire:",
+        choices: ["Dover", "Keene", "Manchester", "Concord"],
+        answer: "Concord"
+    },
+    {
+        title: "Capital Of Maine",
+        choices: ["Sanford", "Newry", "Portland", "Augusta"],
+        answer: "Augusta"
+    }
+]
